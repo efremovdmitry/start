65fd95e965bf9dfd8e1bb445015f1dd186fabe60
defrm <efremov.dmitry.gl@gmail.com>
2024-02-08 00:51:05 +0500

    Создал новую ветку для переноса кода

--- php/index.php
+++ php/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- ../dist/style.css -->
     <link rel="stylesheet" href="../dist/style.css">
-    <title>Главная страница</title>
+    <title>Страница Услуги</title>
 </head>
 
 <body>
